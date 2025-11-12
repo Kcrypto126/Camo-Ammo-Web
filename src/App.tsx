6b4318081e8ab83b62aeaea443689ce13a1e2ee0
kcrypto126 <technt22@gmail.com>
2025-11-13 01:57:40 +0900

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 export default function App() {
+  console.log("AUTHORITY", import.meta.env.VITE_HERCULES_OIDC_AUTHORITY);
+  console.log("CLIENT_ID", import.meta.env.VITE_HERCULES_OIDC_CLIENT_ID);
+  console.log("PROMPT", import.meta.env.VITE_HERCULES_OIDC_PROMPT);
+  console.log(
+    "RESPONSE_TYPE",
+    import.meta.env.VITE_HERCULES_OIDC_RESPONSE_TYPE,
+  );
+  console.log("SCOPE", import.meta.env.VITE_HERCULES_OIDC_SCOPE);
+  console.log("REDIRECT_URI", import.meta.env.VITE_HERCULES_OIDC_REDIRECT_URI);
+
   return (
     <DefaultProviders>
       <BrowserRouter>
